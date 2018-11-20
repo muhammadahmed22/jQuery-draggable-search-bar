22c6b9c816bcdbb35b19c5c0adc070d95334484e
muhammadahmed <muhammadahmed22@gmail.com>
2018-11-20 11:30:41 +0400

    Draggable position fix

--- js/dragging.js
+++ js/dragging.js
@@ -2,27 +2,45 @@
 // Drag container element on mouse action
 // --------------------------------------------------------------------
 
-function endMove() {
-  $(this).removeClass('movable');
-}
+(function($) {
+  $.fn.drags = function(opt) {
 
-function startMove() {
-  $('div.movable').on('mousemove', function (event) {
-    let thisX = event.pageX - $(this).width() / 2,
-      thisY = event.pageY - $(this).height() / 2;
+    opt = $.extend({handle:"",cursor:"move"}, opt);
 
-    $('.movable').offset({
-      left: thisX,
-      top: thisY
+    if(opt.handle === "") {
+      var $el = this;
+    } else {
+      var $el = this.find(opt.handle);
+    }
+
+    return $el.css('cursor', opt.cursor).on("mousedown", function(e) {
+      if(opt.handle === "") {
+        var $drag = $(this).addClass('draggable');
+      } else {
+        var $drag = $(this).addClass('active-handle').parent().addClass('draggable');
+      }
+      var z_idx = $drag.css('z-index'),
+        drg_h = $drag.outerHeight(),
+        drg_w = $drag.outerWidth(),
+        pos_y = $drag.offset().top + drg_h - e.pageY,
+        pos_x = $drag.offset().left + drg_w - e.pageX;
+      $drag.css('z-index', 1000).parents().on("mousemove", function(e) {
+        $('.draggable').offset({
+          top:e.pageY + pos_y - drg_h,
+          left:e.pageX + pos_x - drg_w
+        }).on("mouseup", function() {
+          $(this).removeClass('draggable').css('z-index', z_idx);
+        });
+      });
+    }).on("mouseup", function() {
+      if(opt.handle === "") {
+        $(this).removeClass('draggable');
+      } else {
+        $(this).removeClass('active-handle').parent().removeClass('draggable');
+      }
     });
-  });
-}
 
-// Move container
-$("#container").on('mousedown', function () {
-  $(this).addClass('movable');
-  startMove();
-}).on('mouseup', function () {
-  $(this).removeClass('movable');
-  endMove();
-});
+  }
+})(jQuery);
+
+$('#container').drags();
