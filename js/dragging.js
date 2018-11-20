34378bdbbe15ce5a387b9ed8d1a6af689f2e5b4c
muhammadahmed <muhammadahmed22@gmail.com>
2018-11-20 10:51:46 +0400

    Draggable position fix

--- js/dragging.js
+++ js/dragging.js
@@ -3,26 +3,26 @@
 // --------------------------------------------------------------------
 
 function endMove() {
-    $(this).removeClass('movable');
+  $(this).removeClass('movable');
 }
+
 function startMove() {
-    $('div.movable').on('mousemove', function(event) {
-        var thisX = event.pageX - $(this).width() / 2,
-            thisY = event.pageY - $(this).height() / 2;
+  $('div.movable').on('mousemove', function (event) {
+    let thisX = event.pageX - $(this).width() / 2,
+      thisY = event.pageY - $(this).height() / 2;
 
-        $('.movable').offset({
-            left: thisX,
-            top: thisY
-        });
+    $('.movable').offset({
+      left: thisX,
+      top: thisY
     });
+  });
 }
-$(document).ready(function() {
-    $("#container").on('mousedown', function() {
-        $(this).addClass('movable');
-        startMove();
-    }).on('mouseup', function() {
-        $(this).removeClass('movable');
-        endMove();
-    });
 
+// Move container
+$("#container").on('mousedown', function () {
+  $(this).addClass('movable');
+  startMove();
+}).on('mouseup', function () {
+  $(this).removeClass('movable');
+  endMove();
 });
